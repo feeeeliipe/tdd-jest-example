813fb8a373d039dc0a5625d0eaaeead48026d94c
Felipe Gonçalves <felipe.rinaldi.goncalves@gmail.com>
2021-01-17 13:56:37 -0300

    [FEAT]: Implementando a função para atender os testes escritos

---
+++ src/students/students.js
@@ -0,0 +1,20 @@
+class Student {
+  sumNotes(notes) {
+    // Verifica se existe alguma nota igual a zero, se existir devolve a soma zerada.
+    const minNote = Math.min(...notes);
+    if (minNote === 0) {
+      return 0;
+    }
+
+    // Faz a soma das notas
+    let sum = notes.reduce((result, note) => {
+      result += note;
+      return result;
+    }, 0);
+
+    // Verifica se o aluno tem mais de 5 notas, caso tenha, as notas devem ser mutiplicadas por dois.
+    return notes.length > 5 ? sum * 2 : sum;
+  }
+}
+
+module.exports = new Student();
--- src/students/students.test.js
+++ src/students/students.test.js
@@ -1,16 +1,18 @@
+const student = require("./students");
+
 describe("Testes referente ao modulo de alunos", () => {
   it("Deve retornar 10 quando as notas forem 5, 3 e 2", () => {
     const notes = [5, 3, 2];
-    expect(student.sum(notes)).toEqual(10);
+    expect(student.sumNotes(notes)).toEqual(10);
   });
 
   it("Deve retornar zero, caso uma das notas do aluno seja zero", () => {
     const notes = [1, 5, 0];
-    expect(student.sum(notes).toEqual(0));
+    expect(student.sumNotes(notes)).toEqual(0);
   });
 
   it("Deve mutiplicar a soma por dois, caso o aluno tiver mais que cinco notas", () => {
-    const notes = [5, 2, 3, 5, 2];
-    expect(student.sum(notes).toEqual(34));
+    const notes = [5, 2, 3, 5, 2, 1];
+    expect(student.sumNotes(notes)).toEqual(36);
   });
 });
